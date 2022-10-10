d953afcddb7b1be91691cb3130c2ff502e81d3e5
pallavi1901 <92847182+pallavi1901@users.noreply.github.com>
2022-10-11 00:36:06 +0530

    Update pattern_rect.cpp

--- patterns_cpp/pattern_rect.cpp
+++ patterns_cpp/pattern_rect.cpp
@@ -9,24 +9,7 @@ int main()
 
     for(int i=1; i<=n; i++)
     {
-        for(int j = 1; j<=n-i; j++)
-        {
-            cout<<" ";
-        }
-        for(int j = 1; j<=n; j++)
-        {
-            if(i==1 || i==n )
-            {
-                cout<<'*';
-            }
-            else if( j==1 || j==n)
-            {
-                cout<<'*';
-            }
-            else
-            {
-                cout<<" ";
-            }
+       
         }
 
         cout<<endl;
