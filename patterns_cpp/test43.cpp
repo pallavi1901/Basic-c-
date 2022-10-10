da50347eb1d41008aa412fd3389c61ccc37268b3
pallavi1901 <92847182+pallavi1901@users.noreply.github.com>
2022-10-11 00:41:09 +0530

    Update test43.cpp

--- patterns_cpp/test43.cpp
+++ patterns_cpp/test43.cpp
@@ -15,14 +15,7 @@ int main()
     }
     else
     {
-        for ( int i = 2; i<=sqrt(n); ++i)
-        {
-            if ( n%i==0)
-            {
-                IsPrime = false;
-                break;
-            }
-        }
+       
     }
     if(IsPrime)
     {
